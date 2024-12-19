9734cc382dffca7fa5d0bb411273b8f61ac99956
Muhaimin Shihab <talk.shihab@gmail.com>
2024-12-19 22:56:49 +0600

    fix homepage responsive issues

--- assets/main.js
+++ assets/main.js
@@ -1,10 +1,10 @@
 // Initialize ScrollReveal with global settings
 ScrollReveal({
     reset: false,
-    duration: 1200,
+    duration: 600,
     easing: 'ease-in-out', // Smooth easing
     distance: '50px',      // Adds a nice slide effect
-    viewOffset: { top: 20, bottom: 20 } // Trigger animations slightly earlier
+    viewOffset: { top: 20, bottom: 20 }, // Trigger animations slightly earlier
 });
 
 // Hero area
@@ -15,12 +15,12 @@ ScrollReveal().reveal('.hero-slider', { delay: 1200, origin: 'right' });
 ScrollReveal().reveal('.hero-next', { delay: 1500, origin: 'top' });
 
 // About area
-ScrollReveal().reveal('.about-title', { delay: 200, origin: 'bottom' });
+ScrollReveal().reveal('.about-title', { delay: 300, origin: 'bottom' });
 ScrollReveal().reveal('.about-desc', { delay: 400, origin: 'bottom' });
 ScrollReveal().reveal('.about-gallery', { delay: 600, origin: 'bottom' });
 
 // Experience area
-ScrollReveal().reveal('.experience-title', { delay: 200, origin: 'left' });
+ScrollReveal().reveal('.experience-title', { delay: 300, origin: 'left' });
 
 const experienceItems = document.querySelectorAll('.experience-item');
 experienceItems.forEach((item, index) => {
@@ -28,7 +28,7 @@ experienceItems.forEach((item, index) => {
 });
 
 // Projects area
-ScrollReveal().reveal('.project-title', { delay: 200, origin: 'bottom' });
+ScrollReveal().reveal('.project-title', { delay: 300, origin: 'bottom' });
 
 const projectItems = document.querySelectorAll('.project_card');
 projectItems.forEach((item, index) => {
@@ -38,10 +38,10 @@ projectItems.forEach((item, index) => {
 ScrollReveal().reveal('.project-more', { delay: 1800, origin: 'top' });
 
 // Contact area
-ScrollReveal().reveal('.contact-title', { delay: 200, origin: 'bottom' });
+ScrollReveal().reveal('.contact-title', { delay: 300, origin: 'bottom' });
 ScrollReveal().reveal('.contact-desc', { delay: 400, origin: 'bottom' });
 
 const contactIcons = document.querySelectorAll('.contact-icon');
 contactIcons.forEach((icon, index) => {
-    ScrollReveal().reveal(icon, { delay: 200 + index * 200, origin: 'bottom' });
+    ScrollReveal().reveal(icon, { delay: 200 + index * 250, origin: 'bottom' });
 });
