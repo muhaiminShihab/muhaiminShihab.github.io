2b0b4ec9625ce2b1944f9d1a9a45d2334b0cb6a6
Muhaimin Shihab <talk.shihab@gmail.com>
2024-12-19 23:14:20 +0600

    add cursor design

---
+++ assets/scrollreveal.js
@@ -0,0 +1,38 @@
+// Hero area
+ScrollReveal().reveal('.hero-img', { delay: 200, origin: 'bottom' });
+ScrollReveal().reveal('.hero-title', { delay: 600, origin: 'bottom' });
+ScrollReveal().reveal('.hero-desc', { delay: 900, origin: 'bottom' });
+ScrollReveal().reveal('.hero-slider', { delay: 1200, origin: 'right' });
+ScrollReveal().reveal('.hero-next', { delay: 1500, origin: 'top' });
+
+// About area
+ScrollReveal().reveal('.about-title', { delay: 300, origin: 'bottom' });
+ScrollReveal().reveal('.about-desc', { delay: 400, origin: 'bottom' });
+ScrollReveal().reveal('.about-gallery', { delay: 600, origin: 'bottom' });
+
+// Experience area
+ScrollReveal().reveal('.experience-title', { delay: 300, origin: 'left' });
+
+const experienceItems = document.querySelectorAll('.experience-item');
+experienceItems.forEach((item, index) => {
+    ScrollReveal().reveal(item, { delay: 200 + index * 250, origin: 'left' });
+});
+
+// Projects area
+ScrollReveal().reveal('.project-title', { delay: 300, origin: 'bottom' });
+
+const projectItems = document.querySelectorAll('.project_card');
+projectItems.forEach((item, index) => {
+    ScrollReveal().reveal(item, { delay: 200 + index * 250, origin: 'bottom' });
+});
+
+ScrollReveal().reveal('.project-more', { delay: 1800, origin: 'top' });
+
+// Contact area
+ScrollReveal().reveal('.contact-title', { delay: 300, origin: 'bottom' });
+ScrollReveal().reveal('.contact-desc', { delay: 400, origin: 'bottom' });
+
+const contactIcons = document.querySelectorAll('.contact-icon');
+contactIcons.forEach((icon, index) => {
+    ScrollReveal().reveal(icon, { delay: 200 + index * 250, origin: 'bottom' });
+});
